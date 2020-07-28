0fcb9b8e9be2fe0fd5e980725cfecb787f8d6e57
Eddy Brown <eddybrown@me.com>
2020-07-28 09:34:04 +0100

    Use exception handler middleware

--- src/CloudTrader.Mines.Service/Exceptions/MineNotFoundException.cs
+++ src/CloudTrader.Mines.Service/Exceptions/MineNotFoundException.cs
@@ -4,6 +4,6 @@ namespace CloudTrader.Mines.Service.Exceptions
 {
     public class MineNotFoundException : Exception
     {
-        public MineNotFoundException(int id) : base($"Mine with id \"{id}\" not found.") { }
+        public MineNotFoundException(int id) : base($"Mine with id '{id}' not found.") { }
     }
 }
