f42f73d62590961197f3312dffd760995b11ee81
Laura Warren <lwarren@scottlogic.com>
2020-09-16 13:48:17 +0100

    Fix mistake

--- test/CloudTrader.Mines.Service.Tests/MineServiceTests.cs
+++ test/CloudTrader.Mines.Service.Tests/MineServiceTests.cs
@@ -145,7 +145,7 @@ namespace CloudTrader.Mines.Service.Tests
                 .Setup(mock => mock.UpdateMine(It.IsAny<MineDbModel>()))
                 .ReturnsAsync(new MineDbModel() { Id = expectedId, Latitude = 1, Longitude = 1, Name = "New", Stock = 1, Temperature = 1, UpdateType = UpdateType.weather });
 
-            var expectedUpdatedMine = new Mine() { Id = expectedId, Coordinates = new GeographicCoordinates(1, 1), Name = "New", Stock = 1, Temperature = 1, UpdateType = UpdateType.weather };
+            var expectedUpdatedMine = new Mine() { Id = expectedId, Coordinates = new GeographicCoordinates(1, 1), Name = "New", Stock = 1, Temperature = 1 };
             var actualUpdatedMine = await mineService
                 .UpdateMine(expectedId, new MineUpdateModel() { Coordinates = new GeographicCoordinates(1, 1), Name = "New", Stock = 1, Temperature = 1, UpdateType = UpdateType.weather });
 
